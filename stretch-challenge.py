072aa1b78639245c54a8c82280c5bb82eadc05ae
Gerald <75090595+cpsullivan@users.noreply.github.com>
2023-06-20 19:52:38 -0600

    Add files via upload

---
+++ stretch-challenge.py
@@ -0,0 +1,17 @@
+def sum_of_digits(num):
+    sum_num = 0
+    num = str(num)
+    for digit in num:
+        digit = int(digit)
+        sum_num += digit
+    return sum_num
+print(sum_of_digits(1234))
+
+def prod_of_digits(other_num):
+    prod_num = 0
+    other_num = str(other_num)
+    for digit in other_num:
+        digit = int(digit)
+        prod_num *= digit
+    return prod_num
+print(prod_of_digits(123))
