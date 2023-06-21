3c905afaf6bd0f7b911ba61d387ff9344949abcd
Gerald <75090595+cpsullivan@users.noreply.github.com>
2023-06-20 20:46:31 -0600

    started adding recursive code

--- stretch-challenge.py
+++ stretch-challenge.py
@@ -1,17 +1,27 @@
+#Loop solution
 def sum_of_digits(num):
     sum_num = 0
     num = str(num)
     for digit in num:
-        digit = int(digit)
-        sum_num += digit
+        sum_num += int(digit)
     return sum_num
 print(sum_of_digits(1234))
 
+#Recursive solution
+def recursive_sum(num):
+    rec_sum_num = 0
+    str_num = str(num)
+    #Recurses forever; revise
+    return rec_sum_num + recursive_sum(str_num)
+    
+    #return rec_sum_num + recursive_sum(digit-1)
+print(recursive_sum(123))
+
+#Loop solution
 def prod_of_digits(other_num):
     prod_num = 1
     other_num = str(other_num)
     for digit in other_num:
-        digit = int(digit)
-        prod_num *= digit
+        prod_num *= int(digit)
     return prod_num
 print(prod_of_digits(1234))
